918aae36c64d90a9aced361b1a975062fb7d835a
n-rdy <giuseppegelardi@icloud.com>
2021-12-14 13:59:57 -0600

    start user and auth routes, verified working

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
 
 dotenv.config()
 const app = express()
@@ -18,9 +20,13 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
 
-app.get('/', (req, res) => {
-  res.send('welcome to home page')
-})
+
+
+// routes
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+
+
 
   
 app.listen(5000, () => {
