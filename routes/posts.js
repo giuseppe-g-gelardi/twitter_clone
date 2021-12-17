21071b604aa066f1ab4663989221b13c1642d09d
n-rdy <giuseppegelardi@icloud.com>
2021-12-16 18:29:53 -0600

    fix post replies and get replies to a post

--- routes/posts.js
+++ routes/posts.js
@@ -4,12 +4,16 @@ const { Post } = require('../models/Post')
 const { Reply } = require('../models/Reply')
 const { User } = require('../models/User')
 
+// ? post requests
+// ? post requests
+// ? post requests
+
 // ! get all {user} posts
 // * VERIFIED WORKING
 router.get('/:id/posts', async (req, res) => {
   try {
     const user = await User.findById(req.params.id)
-    res.status(200).json(user.posts)
+    res.status(200).send(user.posts)
   } catch (err) {
     res.status(500).json(err)
   }
@@ -32,7 +36,6 @@ router.get('/:userId/posts/:postId', async (req, res) => {
 // * VERIFIED WORKING
 router.delete('/:userId/posts/:postId', async (req, res) => {
   try {
-    
     const user = await User.findById(req.params.userId)
     if (!user)
       return res
@@ -49,8 +52,7 @@ router.delete('/:userId/posts/:postId', async (req, res) => {
 
     await user.save()
     return res.send(user)
-    
-
+  
   } catch(err) {
     return res.status(500).send(`Internal Server Error: ${ex}`);
   }
@@ -80,62 +82,15 @@ router.post('/:userId', async (req, res) => {
   }
 })
 
-// // update a post
-// // ! may remove function to keep in like with twitter 
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.id)
-//     if (post.userId === req.body.userId) {
-//       await post.updateOne({ $set: req.body })
-//       res.status(200).json('your post has been updated')
-//     } else {
-//       res.status(403).json('you can only update your posts')
-//     }
-//   } catch(err) {
-//     res.status(500).json(err)
-//   }
-// })
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
 
-
-
-
-//post a reply
-// router.post("/:userId/:postId", async (req, res) => {
-//   try {
-
-//     const post = await Post.findById(req.params.postId);
-//     if (!post)
-//       return res
-//         .status(400)
-//         .send(`The post with id "${req.params.postId}" does not exist.`);
-
-//     const reply = new Reply({
-//         text: req.body.text,
-//     });
-
-//     post.replies.push(reply);
-
-//     await post.save();
-//     return res.send(post.replies);
-//   } catch (ex) {
-//     return res.status(500).send(`Internal Server Error: ${ex}`);
-//   }
-// });
-
-// // get a post
-// router.get('/:id', async (req, res) => {
-//   try{
-//     const post = await Post.findById(req.params.id)
-//     res.status(200).json(post)
-//   } catch (err) {
-//     res.status(500).json(err)
-//   }
-// })
-
-
-
-
-
+// * like / unlike post
 
 // // like a post
 // router.put("/:id/like", async (req, res) => {
@@ -153,51 +108,82 @@ router.post('/:userId', async (req, res) => {
 //   }
 // });
 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+
+// ? replies
+// ? replies
+// ? replies
+
+// ! get all replies to a post
+// * VERIFIED WORKING
+router.get('/:userId/posts/:postId/replies', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId)
+    if (!user)
+      return res
+        .status(400)
+        .send(`The user with id "${req.params.userId}" does not exist`)
+
+    let post = user.posts.id(req.params.postId)
+    if (!post)
+      return res
+        .status(400)
+        .send(`The post with id "${req.params.postId}" does not exist`)    
+
+    let replies = post.replies
+
+    res.status(200).send(replies)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// ! post new reply to a post
+// * VERIFIED WORKING
+router.post('/:userId/posts/:postId/replies', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId)
+    if (!user)
+      return res
+        .status(400)
+        .send(`The user with id "${req.params.userId}" does not exist`)
+
+    let post = user.posts.id(req.params.postId)
+    if (!post)
+      return res
+        .status(400)
+        .send(`The post with id "${req.params.postId}" does not exist`)
+
+    let reply = new Reply({
+      text: req.body.text
+    })
+    
+    post = await post.replies.push(reply)
+
+    await user.save()
+    return res.send(user)
+  
+  } catch(err) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+})
+
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
+// ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // ! // 
 
 
 
-
-
-// // TODO replies ?
-
-// //post a reply
-// router.post("/:postId/replies/", async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.postId);
-//     if (!post)
-//       return res
-//         .status(400)
-//         .send(`The post with id "${req.params.postId}" does not exist.`);
-
-//     const reply = new Reply({
-//         text: req.body.text,
-//     });
-
-//     post.replies.push(reply);
-
-//     await post.save();
-//     return res.send(post.replies);
-//   } catch (ex) {
-//     return res.status(500).send(`Internal Server Error: ${ex}`);
-//   }
-// });
-
-//gets all replies of a comment
-// router.get("/:postId/replies/", async (req, res) => {
-//   try {
-//       const post = await Post.findById(req.params.postId);
-//       if (!post)
-//         return res
-//           .status(400)
-//           .send(`The post with id "${req.params.postId}" does not exist.`);
-//   const reply = await Reply.find();
-//     return res.send(post.replies);
-//   } catch (ex) {
-//     return res.status(500).send(`Internal Server Error: ${ex}`);
-//   }
-// });
 
 
 module.exports = router
-
-
