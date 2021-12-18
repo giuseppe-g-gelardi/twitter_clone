6e6e41e7213d2448ac4e42a10e13f680f27e6c15
n-rdy <giuseppegelardi@icloud.com>
2021-12-18 13:24:05 -0600

    change component names, create layout component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { ThemeProvider, StylesProvider } from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
-import { darkTheme, lightTheme } from './components/theme/Theme'
-import { UserProvider } from './context/UserContext'
-import Routing from './pages/Routing'
-
+// import { darkTheme, lightTheme as theme } from './components/theme/Theme'
+import { lightTheme as theme} from './components/theme/Theme'
+import { UserProvider, useAuth } from './context/UserContext'
+import Routing from './components/Routing'
 
 export default function App() {
+  const auth = useAuth()
+  // const [darkMode, setDarkMode] = useState(false)
+  // const [isAuth, setIsAuth] = useState(false)
+  // const theme = darkMode ? darkTheme : lightTheme
 
-  const [darkMode, setDarkMode] = useState(false)
-  const [isAuth, setIsAuth] = useState(false)
-  const theme = darkMode ? darkTheme : lightTheme
+  // const checkAuth = () => {
+  //   const token = localStorage.getItem('token')
+  //   setIsAuth(token !== null)
+  //   console.log('is Auth: ', isAuth)
+  // }
+
+  // useEffect(() => {
+  //   checkAuth()
+  // }, [isAuth])
 
   const checkAuth = () => {
-    const token = localStorage.getItem('token')
-    setIsAuth(token !== null)
-    console.log('is Auth: ', isAuth)
+    console.log('is Auth: ', auth)
   }
 
   useEffect(() => {
     checkAuth()
-  }, [isAuth])
+  }, [auth])
 
   return (
     <UserProvider>
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,5 @@
+import { Container } from "@material-ui/core";
+
+export default function Layout({ children }) {
+  return <Container>{children}</Container>
+}
